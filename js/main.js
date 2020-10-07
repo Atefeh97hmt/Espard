87c669f7f2c91d44b17a55165dc3623caf8780c4
atefeh97hmt <hemmati.atefeh97@gmail.com>
2020-10-07 18:04:11 +0330

    scroll-down-btn

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 200
+			}, 400);
+			return false;
+		});
+});
+
+
+
+
+
