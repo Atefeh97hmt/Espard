f8ebbd8ce5827484b0d5854c06733fb0231f9da5
atefeh97hmt <hemmati.atefeh97@gmail.com>
2020-10-09 14:21:02 +0330

    top to div orange btn designed

--- js/main.js
+++ js/main.js
@@ -1,22 +1,14 @@
 
-$(document).ready(function(){
-	$(window).scroll(function () {
-			if ($(this).scrollTop() > 50) {
-				$('#back-to-top').fadeIn();
-			} else {
-				$('#back-to-top').fadeOut();
-			}
-		});
-		// scroll body to 0px on click
-		$('#back-to-top').click(function () {
-			$('body,html').animate({
-				scrollTop: 200
-			}, 400);
-			return false;
-		});
+
+$(document).ready(function () {
+	$('div.top').click(function() {
+	$('html, body').animate({
+	  scrollTop: $("div.x").offset().top
+	}, 1000)
+  
 });
 
-
+  });
 
 
 
