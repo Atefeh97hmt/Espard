6d469bd177553b9f54455773f9629eee54506e55
atefeh97hmt <hemmati.atefeh97@gmail.com>
2020-10-10 12:09:08 +0330

    slick responsive

--- js/main.js
+++ js/main.js
@@ -6,8 +6,46 @@ $(document).ready(function () {
 	  scrollTop: $("div.top-to-div").offset().top
 	}, 1000)
   
-});
-
+}); 
+	$(".regular").slick({
+		// dots: true,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		speed: 900,
+		responsive: [
+			{
+			  breakpoint: 768,
+			  settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 2
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '10px',
+				slidesToShow: 1
+			  }
+			}
+		  ]
+	});
+	$(".center").slick({
+		dots: true,
+		infinite: true,
+		centerMode: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		speed: 900
+	}); 
   });
 
 
